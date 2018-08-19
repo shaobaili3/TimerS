2d48327f691f66ae502d4de440d4a11630ff5556
Shaobai Li <shaobaili3@gmail.com>
2018-08-20 04:29:02 +1000

    Using #function for UserDefaults

--- TimeS/Models/TimeStatusModel.swift
+++ TimeS/Models/TimeStatusModel.swift
@@ -12,54 +12,54 @@ final class TimeStatusModel {
 
     var targetTime: Int {
         get {
-            return UserDefaults.standard.integer(forKey: "targetTime")
+            return UserDefaults.standard.integer(forKey: #function)
         }
 
         set {
-            UserDefaults.standard.set(newValue, forKey: "targetTime")
+            UserDefaults.standard.set(newValue, forKey: #function)
         }
     }
 
     var startTime: Date {
         get {
-            if let time = UserDefaults.standard.object(forKey: "startTime") as? Date {
+            if let time = UserDefaults.standard.object(forKey: #function) as? Date {
                 return time
             }
             return Date()
         }
 
         set {
-            UserDefaults.standard.set(newValue, forKey: "startTime")
+            UserDefaults.standard.set(newValue, forKey: #function)
         }
     }
 
     var stopWatchIsOn: Bool {
         get {
-            return UserDefaults.standard.bool(forKey: "stopWatchIsOn")
+            return UserDefaults.standard.bool(forKey: #function)
         }
 
         set {
-            UserDefaults.standard.set(newValue, forKey: "stopWatchIsOn")
+            UserDefaults.standard.set(newValue, forKey: #function)
         }
     }
 
     var totalTime: Double {
         get {
-            return UserDefaults.standard.double(forKey: "totalTime")
+            return UserDefaults.standard.double(forKey: #function)
         }
 
         set {
-            UserDefaults.standard.set(newValue, forKey: "totalTime")
+            UserDefaults.standard.set(newValue, forKey: #function)
         }
     }
 
     var timerTag: Int {
         get {
-            return UserDefaults.standard.integer(forKey: "timerTag")
+            return UserDefaults.standard.integer(forKey: #function)
         }
 
         set {
-            UserDefaults.standard.set(newValue, forKey: "timerTag")
+            UserDefaults.standard.set(newValue, forKey: #function)
         }
     }
 }
